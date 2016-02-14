3e02111495a095d7c1354b6c4fe8ebe51903452d
mjasek114 <meganjasek@ischool.berkeley.edu>
2016-02-13 16:11:31 -0800

    Initial answers to questions

--- Homework3/Homework3.R
+++ Homework3/Homework3.R
@@ -9,10 +9,9 @@ desc
 #Question1
 #Analyze some of the variables
 
-# ??Do we need to clean any vars??
-data$mrate.clean = data$mrate*100
-data$mrate.clean[data$mrate.clean>100]=NA
-summary(data$mrate.clean)
+summary(data$lwage)
+print(quantile(data$lwage, probs = c(.01, .05, .10, .25, .50, .75, .90, .95, .99, 1)))
+hist(data$lwage, 50, ylim=c(0,350))
 
 summary(data$jc)
 print(quantile(data$jc, probs = c(.01, .05, .10, .25, .50, .75, .90, .95, .99, 1)))
@@ -20,11 +19,11 @@ hist(data$jc, 50)
 
 summary(data$univ)
 print(quantile(data$univ, probs = c(.01, .05, .10, .25, .50, .75, .90, .95, .99, 1)))
-hist(data$univ, 50)
+hist(data$univ, 50, xlim=c(0,8))
 
 summary(data$exper)
 print(quantile(data$exper, probs = c(.01, .05, .10, .25, .50, .75, .90, .95, .99, 1)))
-hist(data$exper, 50)
+hist(data$exper, 50, xlim=c(0,200))
 
 summary(data$black)
 print(quantile(data$black, probs = c(.01, .05, .10, .25, .50, .75, .90, .95, .99, 1)))
@@ -40,29 +39,47 @@ hist(data$AA, 50)
 
 summary(data$BA)
 print(quantile(data$BA, probs = c(.01, .05, .10, .25, .50, .75, .90, .95, .99, 1)))
-hist(data$BA, 50)
+hist(data$BA, 50, ylim=c(0,5000))
 
 #Question 2
 #Interpret the coefficients B4 (black) and B8 (exper*black).
 data$experXblack = data$exper*data$black
 ols.lwage.8ind = lm(lwage~jc+univ+exper+black+hispanic+AA+BA+experXblack, data=data)
 summary(ols.lwage.8ind)
+plot(ols.lwage.8ind)
+#zero-conditional mean seems to be met
+#homoskedasticity seems to be met
+#assuming random sample
+#assuming linear relationship
 
 #Question3
 # ??are all the assumptions met to do this?  what are the std err like?
 # With this model, test that the return to university education is 7%.
 # H0:  B2=.07
-# t = (B2-H0)/(se/sqrt(n-k-1))
-(.0733 - .07) / ((.0031) / sqrt(6763-8-1))
-# t = 87.48483
-
+# H1:  B2!=.07 (two-tailed)
+# t = (B2-H0)/(se)
+(.0733 - .07) / (.0031)
+2*(1-.8554)
+# t = 1.064516, p-value=2*(1-.8554)=0.2892
+# can't reject the null at 0.05% level
+             
 #Question 4
 #With this model, test that the return to junior college education is equal for black
 #and non-black.
 
 #Question 5
 #With this model, test whether the return to university education is equal to the
-#return to 1 year of working experience.
+#return to 1 year (or 12 months?) of working experience.
+# or do we need to scale everything by 12??
+data$experYr = data$exper/12
+data$univ_plus_experYr = data$univ + data$experYr
+summary(data$univ_plus_experYr)
+hist(data$univ_plus_experYr, breaks=50, xlim=c(0,25))
+# don't forget to take out the exper var otherwise it's a perfect linear combination
+ols.lwage.univ.experYr = lm(lwage~jc+univ+univ_plus_experYr+black+hispanic+AA+BA+experXblack, data=data)
+summary(ols.lwage.univ.experYr)
+#univ represents the difference between univ and exper12.
+#univ coeff is significant, so we can reject the null hypothesis that they are the same.
 
 #Question 6
 #Test the overall signiffcance of this regression.
@@ -84,6 +101,11 @@ R
 data$experXexper = data$exper*data$exper
 ols.lwage.9ind = lm(lwage~jc+univ+exper+black+hispanic+AA+BA+experXblack+experXexper, data=data)
 summary(ols.lwage.9ind)
+#dlwage/dexper = (.004301 - .001239*black + 2*.000003379*exper)
+#dwage = exp((.004301 - .001239*black + 2*.000003379*exper))??
+#exper coeff = 4.301e-03= .004301
+#black coeff = .001239
+#experXexper coeff = 000003379
 
 #Question 8
 #Provide the diagnosis of the homoskedasticity assumption. Does this assumption
